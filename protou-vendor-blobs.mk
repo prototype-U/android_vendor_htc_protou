d6115ea656f12c639a794c84920db9903219a406
atis112 <toth.ati13@gmail.com>
2013-06-30 15:38:54 +0200

    Update

--- protou-vendor-blobs.mk
+++ protou-vendor-blobs.mk
@@ -16,106 +16,48 @@
 
 PRODUCT_COPY_FILES += \
     vendor/htc/protou/proprietary/bin/htc_ebdlogd:system/bin/htc_ebdlogd \
+    vendor/htc/protou/proprietary/bin/mm-jpeg-dec-test:system/bin/mm-jpeg-dec-test \
     vendor/htc/protou/proprietary/bin/mm-qcamera-app:system/bin/mm-qcamera-app \
     vendor/htc/protou/proprietary/bin/awb_camera:system/bin/awb_camera \
     vendor/htc/protou/proprietary/bin/mm-vdec-omx-test:system/bin/mm-vdec-omx-test \
+    vendor/htc/protou/proprietary/bin/mm-jpeg-enc-test:system/bin/mm-jpeg-enc-test \
     vendor/htc/protou/proprietary/bin/cand:system/bin/cand \
-    vendor/htc/protou/proprietary/bin/mm-pp-daemon:system/bin/mm-pp-daemon \
     vendor/htc/protou/proprietary/bin/lsc_camera:system/bin/lsc_camera \
     vendor/htc/protou/proprietary/bin/RescueStarter:system/bin/RescueStarter \
-    vendor/htc/protou/proprietary/bin/charging:system/bin/charging \
     vendor/htc/protou/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
-    vendor/htc/protou/proprietary/bin/mm-adspsvc-test:system/bin/mm-adspsvc-test \
+    vendor/htc/protou/proprietary/bin/mm-qcamera-test:system/bin/mm-qcamera-test \
     vendor/htc/protou/proprietary/bin/htcfs:system/bin/htcfs \
+    vendor/htc/protou/proprietary/bin/mm-qcamera-testsuite-client:system/bin/mm-qcamera-testsuite-client \
     vendor/htc/protou/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/htc/protou/proprietary/bin/mm-venc-omx-test:system/bin/mm-venc-omx-test \
+    vendor/htc/protou/proprietary/bin/test_gemini:system/bin/test_gemini \
     vendor/htc/protou/proprietary/bin/RescueServer:system/bin/RescueServer \
-    vendor/htc/protou/proprietary/bin/btld:system/bin/btld \
-    vendor/htc/protou/proprietary/usr/keychars/qwerty2.kcm:system/usr/keychars/qwerty2.kcm \
-    vendor/htc/protou/proprietary/usr/keychars/Generic.kcm:system/usr/keychars/Generic.kcm \
-    vendor/htc/protou/proprietary/usr/keychars/Virtual.kcm:system/usr/keychars/Virtual.kcm \
-    vendor/htc/protou/proprietary/usr/keychars/qwerty.kcm:system/usr/keychars/qwerty.kcm \
-    vendor/htc/protou/proprietary/usr/keylayout/himax-touchscreen.kl:system/usr/keylayout/himax-touchscreen.kl \
-    vendor/htc/protou/proprietary/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    vendor/htc/protou/proprietary/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    vendor/htc/protou/proprietary/usr/keylayout/sr_touchscreen.kl:system/usr/keylayout/sr_touchscreen.kl \
-    vendor/htc/protou/proprietary/usr/keylayout/projector-Keypad.kl:system/usr/keylayout/projector-Keypad.kl \
-    vendor/htc/protou/proprietary/usr/idc/projector_input.idc:system/usr/idc/projector_input.idc \
-    vendor/htc/protou/proprietary/usr/idc/himax-touchscreen.idc:system/usr/idc/himax-touchscreen.idc \
-    vendor/htc/protou/proprietary/etc/soundimage/srsfx_trumedia_51.cfg:system/etc/soundimage/srsfx_trumedia_51.cfg \
-    vendor/htc/protou/proprietary/etc/soundimage/srs_geq10.cfg:system/etc/soundimage/srs_geq10.cfg \
-    vendor/htc/protou/proprietary/etc/soundimage/srsfx_trumedia_movie.cfg:system/etc/soundimage/srsfx_trumedia_movie.cfg \
-    vendor/htc/protou/proprietary/etc/soundimage/srsfx_trumedia_voice.cfg:system/etc/soundimage/srsfx_trumedia_voice.cfg \
-    vendor/htc/protou/proprietary/etc/soundimage/srsfx_trumedia_music.cfg:system/etc/soundimage/srsfx_trumedia_music.cfg \
-    vendor/htc/protou/proprietary/etc/soundimage/srs_global.cfg:system/etc/soundimage/srs_global.cfg \
-    vendor/htc/protou/proprietary/etc/soundimage/srs_bypass.cfg:system/etc/soundimage/srs_bypass.cfg \
-    vendor/htc/protou/proprietary/etc/calibration:system/etc/calibration \
-    vendor/htc/protou/proprietary/etc/firmware/fw_bcm4330_p2p_b2.bin:system/etc/firmware/fw_bcm4330_p2p_b2.bin \
-    vendor/htc/protou/proprietary/etc/firmware/fw_bcm4330_b2.bin:system/etc/firmware/fw_bcm4330_b2.bin \
-    vendor/htc/protou/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-    vendor/htc/protou/proprietary/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
-    vendor/htc/protou/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
-    vendor/htc/protou/proprietary/etc/firmware/fw_bcm4330_apsta_b2.bin:system/etc/firmware/fw_bcm4330_apsta_b2.bin \
-    vendor/htc/protou/proprietary/etc/firmware/BCM4330B1_002.001.003.0833.0845.hcd:system/etc/firmware/BCM4330B1_002.001.003.0833.0845.hcd \
-    vendor/htc/protou/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    vendor/htc/protou/proprietary/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov8838_hfr.so:system/lib/libchromatix_ov8838_hfr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov8838_preview.so:system/lib/libchromatix_ov8838_preview.so \
+    vendor/htc/protou/proprietary/bin/mm-jpeg-enc-test-client:system/bin/mm-jpeg-enc-test-client \
     vendor/htc/protou/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3h2yx_hfr.so:system/lib/libchromatix_s5k3h2yx_hfr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov5693_zsl.so:system/lib/libchromatix_ov5693_zsl.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov8838_hdr.so:system/lib/libchromatix_ov8838_hdr.so \
     vendor/htc/protou/proprietary/lib/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx091_preview.so:system/lib/libchromatix_imx091_preview.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx074_preview.so:system/lib/libchromatix_imx074_preview.so \
     vendor/htc/protou/proprietary/lib/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3l1yx_default_video.so:system/lib/libchromatix_s5k3l1yx_default_video.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3l1yx_hfr_60fps.so:system/lib/libchromatix_s5k3l1yx_hfr_60fps.so \
     vendor/htc/protou/proprietary/lib/libchromatix_ov5693_preview.so:system/lib/libchromatix_ov5693_preview.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k4e5yx_zsl.so:system/lib/libchromatix_s5k4e5yx_zsl.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k6a1gx_hfr.so:system/lib/libchromatix_s5k6a1gx_hfr.so \
-    vendor/htc/protou/proprietary/lib/libcameraasd.so:system/lib/libcameraasd.so \
     vendor/htc/protou/proprietary/lib/libmmcamera_hdr_lib.so:system/lib/libmmcamera_hdr_lib.so \
     vendor/htc/protou/proprietary/lib/libimage-jpeg-enc-omx-comp.so:system/lib/libimage-jpeg-enc-omx-comp.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx074_video_hd.so:system/lib/libchromatix_imx074_video_hd.so \
     vendor/htc/protou/proprietary/lib/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx135_hfr.so:system/lib/libchromatix_imx135_hfr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov8838_zsl.so:system/lib/libchromatix_ov8838_zsl.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ar0260_zsl.so:system/lib/libchromatix_ar0260_zsl.so \
     vendor/htc/protou/proprietary/lib/libimage-omx-common.so:system/lib/libimage-omx-common.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx175_default_video.so:system/lib/libchromatix_imx175_default_video.so \
+    vendor/htc/protou/proprietary/lib/libgemini.so:system/lib/libgemini.so \
     vendor/htc/protou/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
     vendor/htc/protou/proprietary/lib/libOlaEngineNew.so:system/lib/libOlaEngineNew.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx091_video_hd.so:system/lib/libchromatix_imx091_video_hd.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k6a2ya_preview.so:system/lib/libchromatix_s5k6a2ya_preview.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_vd6869_hdr.so:system/lib/libchromatix_vd6869_hdr.so \
+    vendor/htc/protou/proprietary/lib/libchromatix_s5k4e1_default_video.so:system/lib/libchromatix_s5k4e1_default_video.so \
     vendor/htc/protou/proprietary/lib/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
     vendor/htc/protou/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k6a2ya_hfr.so:system/lib/libchromatix_s5k6a2ya_hfr.so \
     vendor/htc/protou/proprietary/lib/libchromatix_ov5693_default_video.so:system/lib/libchromatix_ov5693_default_video.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k6a2ya_default_video.so:system/lib/libchromatix_s5k6a2ya_default_video.so \
     vendor/htc/protou/proprietary/lib/libmmcamera_image_stab.so:system/lib/libmmcamera_image_stab.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k6a1gx_preview.so:system/lib/libchromatix_s5k6a1gx_preview.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx175_zsl.so:system/lib/libchromatix_imx175_zsl.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx074_default_video.so:system/lib/libchromatix_imx074_default_video.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx135_preview.so:system/lib/libchromatix_imx135_preview.so \
+    vendor/htc/protou/proprietary/lib/libwvdrm_L3.so:system/lib/libwvdrm_L3.so \
     vendor/htc/protou/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_vd6869_preview.so:system/lib/libchromatix_vd6869_preview.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k6a2ya_hdr.so:system/lib/libchromatix_s5k6a2ya_hdr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov5693_hfr.so:system/lib/libchromatix_ov5693_hfr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3h2yx_preview.so:system/lib/libchromatix_s5k3h2yx_preview.so \
-    vendor/htc/protou/proprietary/lib/libdivxdrmdecrypt.so:system/lib/libdivxdrmdecrypt.so \
+    vendor/htc/protou/proprietary/lib/libchromatix_s5k4e1_preview.so:system/lib/libchromatix_s5k4e1_preview.so \
     vendor/htc/protou/proprietary/lib/libgsl.so:system/lib/libgsl.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx175_hfr.so:system/lib/libchromatix_imx175_hfr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov2722_preview.so:system/lib/libchromatix_ov2722_preview.so \
     vendor/htc/protou/proprietary/lib/libmmcamera_frameproc.so:system/lib/libmmcamera_frameproc.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx135_default_video.so:system/lib/libchromatix_imx135_default_video.so \
     vendor/htc/protou/proprietary/lib/libmmstillomx.so:system/lib/libmmstillomx.so \
-    vendor/htc/protou/proprietary/lib/libmm-abl-oem.so:system/lib/libmm-abl-oem.so \
-    vendor/htc/protou/proprietary/lib/libcam_oem_plugin.so:system/lib/libcam_oem_plugin.so \
+    vendor/htc/protou/proprietary/lib/libDxOAF.so:system/lib/libDxOAF.so \
     vendor/htc/protou/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
     vendor/htc/protou/proprietary/lib/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
-    vendor/htc/protou/proprietary/lib/libhtccamera.so:system/lib/libhtccamera.so \
     vendor/htc/protou/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
     vendor/htc/protou/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
     vendor/htc/protou/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
@@ -123,55 +65,29 @@ PRODUCT_COPY_FILES += \
     vendor/htc/protou/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/htc/protou/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
     vendor/htc/protou/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3l1yx_hfr_90fps.so:system/lib/libchromatix_s5k3l1yx_hfr_90fps.so \
     vendor/htc/protou/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_vd6869_default_video.so:system/lib/libchromatix_vd6869_default_video.so \
     vendor/htc/protou/proprietary/lib/libchromatix_s5k4e5yx_default_video.so:system/lib/libchromatix_s5k4e5yx_default_video.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k6a1gx_hdr.so:system/lib/libchromatix_s5k6a1gx_hdr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov2720_preview.so:system/lib/libchromatix_ov2720_preview.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k6a1gx_default_video.so:system/lib/libchromatix_s5k6a1gx_default_video.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k4e5yx_hfr.so:system/lib/libchromatix_s5k4e5yx_hfr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx135_zsl.so:system/lib/libchromatix_imx135_zsl.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k6a1gx_zsl.so:system/lib/libchromatix_s5k6a1gx_zsl.so \
+    vendor/htc/protou/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
+    vendor/htc/protou/proprietary/lib/libWVStreamControlAPI_L3.so:system/lib/libWVStreamControlAPI_L3.so \
     vendor/htc/protou/proprietary/lib/libhtc_ril.so:system/lib/libhtc_ril.so \
     vendor/htc/protou/proprietary/lib/libcamerapp.so:system/lib/libcamerapp.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx091_default_video.so:system/lib/libchromatix_imx091_default_video.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx091_zsl.so:system/lib/libchromatix_imx091_zsl.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_vd6869_hfr.so:system/lib/libchromatix_vd6869_hfr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3h2yx_default_video.so:system/lib/libchromatix_s5k3h2yx_default_video.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov5647_preview.so:system/lib/libchromatix_ov5647_preview.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ar0260_default_video.so:system/lib/libchromatix_ar0260_default_video.so \
+    vendor/htc/protou/proprietary/lib/libmmparser_divxdrmlib.so:system/lib/libmmparser_divxdrmlib.so \
+    vendor/htc/protou/proprietary/lib/libOlaEngine.so:system/lib/libOlaEngine.so \
+    vendor/htc/protou/proprietary/lib/libWVphoneAPI.so:system/lib/libWVphoneAPI.so \
+    vendor/htc/protou/proprietary/lib/libwvm.so:system/lib/libwvm.so \
+    vendor/htc/protou/proprietary/lib/libposteffect.so:system/lib/libposteffect.so \
     vendor/htc/protou/proprietary/lib/libchromatix_s5k4e5yx_hdr.so:system/lib/libchromatix_s5k4e5yx_hdr.so \
     vendor/htc/protou/proprietary/lib/libmmcamera_faceproc.so:system/lib/libmmcamera_faceproc.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov8838_default_video.so:system/lib/libchromatix_ov8838_default_video.so \
     vendor/htc/protou/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx074_zsl.so:system/lib/libchromatix_imx074_zsl.so \
-    vendor/htc/protou/proprietary/lib/libmm-abl.so:system/lib/libmm-abl.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov2720_default_video.so:system/lib/libchromatix_ov2720_default_video.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx175_preview.so:system/lib/libchromatix_imx175_preview.so \
     vendor/htc/protou/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so \
     vendor/htc/protou/proprietary/lib/hw/camera.default.so:system/lib/hw/camera.default.so \
     vendor/htc/protou/proprietary/lib/libmmcamera_statsproc31.so:system/lib/libmmcamera_statsproc31.so \
     vendor/htc/protou/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ar0260_preview.so:system/lib/libchromatix_ar0260_preview.so \
     vendor/htc/protou/proprietary/lib/libchromatix_s5k4e5yx_preview.so:system/lib/libchromatix_s5k4e5yx_preview.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3l1yx_video_hd.so:system/lib/libchromatix_s5k3l1yx_video_hd.so \
-    vendor/htc/protou/proprietary/lib/libBeautyChat.so:system/lib/libBeautyChat.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3h2yx_hdr.so:system/lib/libchromatix_s5k3h2yx_hdr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov2722_zsl.so:system/lib/libchromatix_ov2722_zsl.so \
-    vendor/htc/protou/proprietary/lib/libHTC_DIS.so:system/lib/libHTC_DIS.so \
     vendor/htc/protou/proprietary/lib/libmmcamera_rawchipproc.so:system/lib/libmmcamera_rawchipproc.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_ov2722_default_video.so:system/lib/libchromatix_ov2722_default_video.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx135_hdr.so:system/lib/libchromatix_imx135_hdr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_vd6869_zsl.so:system/lib/libchromatix_vd6869_zsl.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_imx175_hdr.so:system/lib/libchromatix_imx175_hdr.so \
+    vendor/htc/protou/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
     vendor/htc/protou/proprietary/lib/libmmcamera_wavelet_lib.so:system/lib/libmmcamera_wavelet_lib.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k6a2ya_zsl.so:system/lib/libchromatix_s5k6a2ya_zsl.so \
     vendor/htc/protou/proprietary/lib/libcameraface.so:system/lib/libcameraface.so \
     vendor/htc/protou/proprietary/lib/libmmcamera_interface2.so:system/lib/libmmcamera_interface2.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3l1yx_zsl.so:system/lib/libchromatix_s5k3l1yx_zsl.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3l1yx_hfr_120fps.so:system/lib/libchromatix_s5k3l1yx_hfr_120fps.so \
     vendor/htc/protou/proprietary/lib/libchromatix_ov5693_hdr.so:system/lib/libchromatix_ov5693_hdr.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3l1yx_preview.so:system/lib/libchromatix_s5k3l1yx_preview.so \
-    vendor/htc/protou/proprietary/lib/libchromatix_s5k3h2yx_zsl.so:system/lib/libchromatix_s5k3h2yx_zsl.so \
     vendor/htc/protou/proprietary/lib/libmmipl.so:system/lib/libmmipl.so
